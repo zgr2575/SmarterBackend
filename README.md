602fb1636ce209fa88c546e7b0aaba44b8f8412c
zgr2575 <62474113+zgr2575@users.noreply.github.com>
2024-05-03 11:47:02 +0000

    V1

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # SmarterBackend
-SmarterBackend is a node based website backend for static webhosts.
-
-Fork this and make changes I have no ideas for where to go with this. 
+Welcome to SmarterBackend, a secure and fast static backend for your HTML projects.
---
+++ static/scripts/sbe.js
@@ -0,0 +1,12 @@
+// Add Branding bc im a jew :p
+/*  <center>
+<h1 style="font-size:38px;font-family: Quicksand, Helvetica, sans-serif;">SmarterBackend</h1>
+<p style="font-size:16px;font-family: Arial, Helvetica, sans-serif;">This site is secured by the most powerful open sourced static backend. </p>
+</center>
+*/
+var sbev = 1;
+var element = document.createElement("sbe");
+element.appendChild(document.createTextNode('The man who mistook his wife for a hat'));
+document.getElementById('sbe').appendChild(element);
+
+doccument.createElemen
